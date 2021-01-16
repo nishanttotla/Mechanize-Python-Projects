ca3d8f5f1a8432a4420c129410e9dc586c9510e9
Nishant Totla <nishanttotla@gmail.com>
2021-01-16 11:21:27 -0800

    Allow a list of case id numbers
    Signed-off-by: Nishant Totla <nishanttotla@gmail.com>

--- examples/uscis-status.py
+++ examples/uscis-status.py
@@ -11,30 +11,25 @@ br.set_handle_redirect(True)
 br.set_handle_referer(True)
 br.set_handle_robots(False)
 
-################ Fill form and submit ################
-
 url = 'https://egov.uscis.gov/casestatus/landing.do'
 form_name = 'caseStatusForm'
-receipt_num = '' # enter receipt number here
+receipt_nums = [] # enter receipt numbers here
 
-response = br.open(url)
-br.select_form(form_name)
+for receipt_num in receipt_nums:
+  ##### Fill form and submit for each receipt_num #####
+  response = br.open(url)
+  br.select_form(form_name)
 
-# Enter info in form
-br.form.set_all_readonly(False)
-br['appReceiptNum'] = receipt_num
-br.submit()
+  # Enter info in form
+  br.form.set_all_readonly(False)
+  br['appReceiptNum'] = receipt_num
+  br.submit()
 
-################ Extract info from the response ################
+  ##### Extract info from the response #####
+  html_doc = br.response().read()
+  soup = BeautifulSoup(html_doc, 'html.parser')
+  case_status = soup.h1.text # first h1 is the status
+  case_status_description = soup.p.text # first p is the description
 
-html_doc = br.response().read()
-soup = BeautifulSoup(html_doc, 'html.parser')
-case_status = soup.h1.text # first h1 is the status
-case_status_description = soup.p.text # first p is the description
-
-print '---' + case_status + '---\n' + case_status_description
-
-################ Send email update ################
-
-# TODO
+  print receipt_num + ': ' + case_status
 
